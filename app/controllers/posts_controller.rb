b558c4906d817f01cef998310ff2c3a1933f6f20
Yusuke Watanabe <981461yus@gmail.com>
2019-05-24 16:44:23 +0900

    post function added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,29 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def create
+    @post = Post.new(
+      id: params[:id],
+      title: params[:title],
+      content: params[:content]
+  )
+
+    if params[:image]
+      @name = params[:image].original_filename
+      #image_nameの名前をデータベースに保存
+      @post.image_name = "#{@name}"
+       #投稿した画像を保存
+      image = params[:image]
+      File.binwrite("public/pic_images/#{@post.image_name}", image.read)
+    end
+
+  	if @post.save
+  	  flash[:notice] = "投稿を作成しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'posts/index'
+  post 'posts/create'
   root 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
