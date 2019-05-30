fb1583370c1927b948b33561bc20cd9e894ea903
Yusuke Watanabe <981461yus@gmail.com>
2019-05-30 13:04:05 +0900

    user image column added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_26_103344) do
+ActiveRecord::Schema.define(version: 2019_05_30_032114) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_05_26_103344) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_name"
   end
 
 end
