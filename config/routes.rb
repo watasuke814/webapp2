162cd71081a939291c5a637e20842108637f6edb
Yusuke Watanabe <981461yus@gmail.com>
2019-06-01 15:45:41 +0900

    user edit added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index,:create]
-  resources :users, only: [:create,:edit] 
+  resource :users, only: [:create,:edit] 
+  post 'users/login'
   root 'home#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
