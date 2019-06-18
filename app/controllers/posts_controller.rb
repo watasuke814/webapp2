e710abbadb4b2e0a220e7afe730613020e5b8634
Yusuke Watanabe <981461yus@gmail.com>
2019-06-18 14:11:07 +0900

    posts function edited

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,11 +7,13 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    binding.pry
     if @post.save
       @post.user_id = @current_user.id
       flash[:success] = "投稿を作成しました"
       redirect_to posts_path
     else
+      @posts = Post.page(params[:page]).per(4).order(created_at: :desc)
       render :index
     end
   end
