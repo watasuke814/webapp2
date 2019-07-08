e617b96df0483cc3c41bd6a6b5b5bf5645c6f97f
Yusuke Watanabe <981461yus@gmail.com>
2019-07-08 16:07:29 +0900

    favorite system added

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,12 +1,12 @@
 class FavoritesController < ApplicationController
   def create
-    favorite = current_user.favorites.build(post_id: params[:post_id])
+    favorite = @current_user.favorites.build(post_id: params[:post_id])
     favorite.save
     redirect_to posts_path
   end
 
   def destroy
-    favorite = Favorite.find_by(tweet_id: params[:post_id], user_id: current_user.id)
+    favorite = Favorite.find_by(post_id: params[:post_id], user_id: @current_user.id)
     favorite.destroy
     redirect_to posts_path
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,6 @@ class Post < ApplicationRecord
     has_many :favorites
 
     def favorited_by?(user)
-        favorites.where(user_id: user.id).exist?
+        favorites.where(user_id: user.id).exists?
     end
 end
